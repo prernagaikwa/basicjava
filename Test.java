4301ec956f9faa72576ea038cf8be1b5cdce5208
prernagaikwa <gaikwadprerna102@gmail.com>
2026-02-04 18:20:07 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+//package com.engineeringdigest.corejava;
+// public class ThreadTest{
+//     public static void main(String[] args) {
+//         System.out.println("Hello World");
+//         System.out.println(Thread.currentThread().getName());
+//     }
+// }
+
+public class Test{
+    public static void main(String[] args){
+    for(int i =0 ; i < 1000 ; i++){
+        System.out.println("Hello");
+    }
+} }
